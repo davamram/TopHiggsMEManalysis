5874c14ce5f4ab79039e7f79ca7eeee15122926c
nchanon <nicolas.pierre.chanon@cern.ch>
2019-03-11 10:11:56 +0100

    Update paths

--- FlatTreeMEMproducer/test/auto.sh
+++ FlatTreeMEMproducer/test/auto.sh
@@ -1,1 +1,1 @@
-./runMEM /afs/cern.ch/user/n/ntonon/public/forNicolas/tHq_2lSS.root 0 1 --MEMRun Production/config.cfg 1
+./runMEM /afs/cern.ch/work/n/ntonon/public/tHq_ntuples/input_ntuples/tHq2017/3l/SR/tHq.root 0 1 --MEMRun Production/config.cfg 1
